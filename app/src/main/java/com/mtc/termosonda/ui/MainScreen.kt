577d0063268a9dbbbb765034dc0d29a623e7003e
estebanc2 <estebanc2@gmail.com>
2026-04-16 10:49:46 -0300

    listo todo ok, subido a google play

--- app/src/main/java/com/mtc/termosonda/ui/MainScreen.kt
+++ app/src/main/java/com/mtc/termosonda/ui/MainScreen.kt
@@ -53,7 +53,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.mtc.termosonda.R
